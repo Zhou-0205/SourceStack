ac113ee3aff72d52834adfaa82d82cc67a70ed35
zdh <2289304056@qq.com>
2020-10-23 21:49:32 +0800

    问题

--- First/Program.cs
+++ First/Program.cs
@@ -5,8 +5,29 @@ namespace First
 {
     class Program
     {
+        //定义一个生成数组的方法：int[] GetArray()，其元素随机生成从小到大排列。
+        //利用可选参数控制：
+        //  最小值min（默认为1）
+        //  相邻两个元素之间的最大差值gap（默认为5）
+        //  元素个数length（默认为10个）
+        public static int[] GetArray(int min = 1, int gap = 5, int length = 10)
+        {
+            int[] array = new int[length];
+            Random ran = new Random();
+            array[0] = ran.Next(min);
+            Console.WriteLine(array[0]);
+            for (int i = 1; i < array.Length; i++)
+            {
+                array[i] = ran.Next(gap) + array[i - 1];
+                Console.WriteLine(array[i]);
+            }
+            return array;
+        }
+
         static void Main(string[] args)
         {
+            GetArray(10,10,100);
+
             //用户依次由控制台输入：验证码、用户名和密码：
             //  如果验证码输入错误，直接输出：“*验证码错误”；
             //  如果用户名不存在，直接输出：“*用户名不存在”；
@@ -120,15 +141,15 @@ namespace First
             //Console.WriteLine(min);
             //生成一个元素（值随机）从小到大排列的数组
             //int[] a = new int[10];
-            //Random ran = new Random();
+            //random ran = new random();
             //for (int i = 0; i < 10; i++)
             //{
-            //    a[i] = ran.Next(100);
+            //    a[i] = ran.next(100);
             //}
-            //Array.Sort(a);
+            //array.sort(a);
             //for (int i = 0; i < 10; i++)
             //{
-            //    Console.WriteLine(a[i]);
+            //    console.writeline(a[i]);
             //}
             //设立并显示一个多维数组的值，元素值等于下标之和
             //static void twoDimensional()
@@ -171,7 +192,7 @@ namespace First
             //    }
             //}
             //计算得到源栈同学的平均成绩（精确到两位小数），方法名GetAverage()
-            //static string GetAverage(double[] scores)
+            //static double GetAverage(double[] scores)
             //{
             //    double sum = 0;
             //    double avg;
@@ -182,7 +203,7 @@ namespace First
             //    avg = sum / scores.Length;
 
             //    avg = Math.Round((sum / scores.Length), 2);
-            //    return $"平均成绩为{avg}";
+            //    return avg;
             //}
             //完成“猜数字”游戏，方法名GuessMe()：
             //  随机生成一个大于0小于1000的整数
@@ -200,15 +221,14 @@ namespace First
             //        input = Convert.ToInt32(Console.ReadLine());
             //        if (input != num)
             //        {
-            //            if (input > num)
+            //            if (i == 10)
+            //            {
+            //                Console.WriteLine("～￣(OO)￣");
+            //            }
+            //            else if (input > num)
             //            {
             //                Console.WriteLine($"太大了!还剩{10 - i}次");
             //                continue;
-            //            }
-            //            else if (i == 10)
-            //            {
-            //                Console.WriteLine("～￣(OO)￣");
-            //                break;
             //            }
             //            else
             //            {
@@ -221,18 +241,16 @@ namespace First
             //        if (i <= 5)
             //        {
             //            Console.WriteLine("你真牛逼");
-            //            break;
             //        }
             //        else if (i <= 8)
             //        {
             //            Console.WriteLine("不错嘛");
-            //            break;
             //        }
             //        else
             //        {
             //            Console.WriteLine("太笨了！");
-            //            break;
             //        }
+            //        break;
             //        }
             //    }
             //}
@@ -248,7 +266,7 @@ namespace First
             //将登陆的过程封装成一个方法LogOn()，调用之后能够获得：
             //  true / false，表示登陆是否成功
             //  string，表示登陆失败的原因
-            //static string logOn(string authCode, string userName, string password)
+            //static bool logOn(string authCode, string userName, string password，output reason)
             //{
             //    Console.WriteLine($"请输入验证码{authCode}");
             //    if (Console.ReadLine() == authCode)
@@ -263,21 +281,22 @@ namespace First
             //            }
             //            else
             //            {
-            //                return "用户名或密码错误";
+            //                out="用户名或密码错误";
             //            }
             //        }
             //        else
             //        {
-            //            return "用户名不存在";
+            //            out= "用户名不存在";
             //        }
             //    }
             //    else
             //    {
-            //        return "验证码错误";
+            //        out= "验证码错误";
             //    }
             //}
 
         }
+
     }
 }
 
