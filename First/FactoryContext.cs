16427387f7f7340b60e2823a310ded1d75fa0cfa
zdh <2289304056@qq.com>
2020-10-29 11:09:58 +0800

    单例

---
+++ First/FactoryContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharplearn
+{
+    public class FactoryContext
+    {
+        public static readonly FactoryContext _instance;
+
+        static FactoryContext()
+        {
+            _instance = new FactoryContext();
+        }
+
+        private FactoryContext()
+        {
+
+        }
+    }
+}
