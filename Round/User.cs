53b89dbb400a5cb9eed1b7d44ab8ecbb598c801a
zdh <2289304056@qq.com>
2020-11-08 21:17:36 +0800

    测试

---
+++ Round/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SELFstudy
+{
+    class User
+    {
+        private static string Name;
+        private static int Password;
+        private static User Invitedby;
+
+        public static void Register()
+        { }
+        public static void Login()
+        { }
+    }
+}
