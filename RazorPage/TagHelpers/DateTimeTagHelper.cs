73cd2f3e4c99da84f44f88f65616622af2daed95
zdh <2289304056@qq.com>
2021-04-20 17:21:36 +0800

    自定义TagHelper

---
+++ RazorPage/TagHelpers/DateTimeTagHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace RazorPage.TagHelpers
+{
+    [HtmlTargetElement("datetime", Attributes = "asp-showicon, asp-only")]
+    public class DateTimeTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "small";
+
+            if (context.AllAttributes["asp-showicon"].Value.ToString() == "true")
+            {
+                output.PreContent.SetHtmlContent("<span class=\"fa fa - calendar\"></span>");
+            }
+            if (context.AllAttributes["asp-only"].Value.ToString() == "date")
+            {
+                output.Content.AppendHtml(
+                    Convert.ToDateTime(
+                        output
+                        .GetChildContentAsync()
+                        .Result
+                        .GetContent())
+                    .ToString("yyyy年MM月dd日"));
+
+            }
+
+            base.Process(context, output);
+        }
+    }
+}
