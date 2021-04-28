2d8497338789c332902cb11297e0599f99ab4bcb
zdh <2289304056@qq.com>
2021-04-28 16:42:37 +0800

    ADO.NET,注册登录

--- RazorPage/Repositories/UserRepository.cs
+++ RazorPage/Repositories/UserRepository.cs
@@ -1,44 +1,97 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
 using RazorPage.Entities;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 
 namespace RazorPage.Repositories
 {
     public class UserRepository
     {
-        private static IList<User> users;
-        public UserRepository()
-        {
-            users = new List<User>
-            {
-                new User{Id=1,Name="张三",Password="1234",Level=1},
-                new User{Id=2,Name="李四",Password="1234",Level=5},
-                new User{Id=3,Name="王五",Password="1234",Level=7},
-                new User{Id=4,Name="赵六",Password="1234",Level=9},
-                new User{Id=5,Name="田七",Password="1234",InvitedCode="1234"},
-            };
-        }
+        private const string Id = "Id";
+        private const string Name = "Name";
+        private const string Password = "Password";
+        private const string InvitedBy = "InvitedBy";
+        private const string InvitedCode = "InvitedCode";
+        private const string BMoney = "BMoney";
+        private const string Level = "Level";
 
+        private const string connectionString = @"Data Source=.;
+                                                Initial Catalog=18bang;
+                                                Integrated security=true;";
+        public User GetByName(string name)
+        {
+            using (DbConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                IDbCommand command = new SqlCommand();
+                command.CommandText =
+                    $"SELECT {Id},{Name},{Password},{InvitedBy},{InvitedCode},{BMoney},{Level}" +
+                    $"FROM [User] " +
+                    $"WHERE [Name] = @Name";
+                command.Connection = connection;
+
+                IDataParameter paramName = new SqlParameter("@Name", name);
+                command.Parameters.Add(paramName);
+
+                IDataReader reader = command.ExecuteReader();
+
+                User user = new User();
+
+                if (!reader.Read())
+                {
+                    return null;
+                }
+                if (reader.Read())
+                {
+                    user.Id = (int)reader["Id"];
+                    user.Name = (string)reader["Name"];
+                    user.Password = (string)reader["Password"];
+                    user.InvitedBy = new User
+                    {
+                        Id = (int)reader["InvitedBy"]
+                    };
+                    user.InvitedCode = (string)reader["InvitedCode"];
+                    user.BMoney = (int)reader["BMoney"];
+                    user.Level = (int)reader["Level"];
+                }
+                else
+                {
+                    return null;
+                }
+                return user;
+            }
+
+        }
+        public void Save(User user)
+        {
+            using (DbConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                IDbCommand command = new SqlCommand();
+                command.CommandText = 
+                    $"INSERT INTO [User]([Name],[Password]) " +
+                    $"VALUES(N'@Name',N'@Passowrd')";
+                command.Connection = connection;
+
+                IDataParameter paramName = new SqlParameter("@Name", user.Name);
+                IDataParameter paramPassword = new SqlParameter("@Passowrd", user.Password);
+
+                command.Parameters.Add(paramName);
+                command.Parameters.Add(paramPassword);
+
+                int rows = command.ExecuteNonQuery();
+            }
+        }
         public User Find(int id)
         {
-            return users.Where(u => u.Id == id).SingleOrDefault();
-        }
-
-        internal IList<User> Get()
-        {
-            return users;
-        }
-
-        internal void Save(User newUser)
-        {
-            users.Add(newUser);
-        }
-
-        internal User GetByName(string name)
-        {
-            return users.Where(u => u.Name == name).SingleOrDefault();
+            User user = new User();
+            return user;
         }
     }
 }
