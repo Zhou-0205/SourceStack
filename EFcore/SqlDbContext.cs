9646e6d6466a9c965563c36437d573b06d9c8af6
zdh <2289304056@qq.com>
2021-05-07 14:38:33 +0800

    配置Logger

--- EFcore/SqlDbContext.cs
+++ EFcore/SqlDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,7 +13,15 @@ namespace EFcore
         {
             string connStr =
                 @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=17bang;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-            optionsBuilder.UseSqlServer(connStr);
+            optionsBuilder.UseSqlServer(connStr)
+#if Debug
+                .EnableSensitiveDataLogging(true)
+#endif
+                .LogTo
+                (
+                    (id, level) => level == LogLevel.Error,
+                    log => Console.WriteLine(log)
+                ); ;
             base.OnConfiguring(optionsBuilder);
         }
     }
